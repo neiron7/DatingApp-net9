bcf5281f679594a2d011df18cc9bd3cecfa2f062
neiron7 <neiron77@gmail.com>
2025-02-16 14:06:33 +0100

    End of section 6

---
+++ client/src/app/messagess/messagess.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-messagess',
+  standalone: true,
+  imports: [],
+  templateUrl: './messagess.component.html',
+  styleUrl: './messagess.component.css'
+})
+export class MessagessComponent {
+
+}
